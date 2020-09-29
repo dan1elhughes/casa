991acb6d2a997241ee0a14be7f898d439ccd87ce
Dan Hughes <danhughes@monzo.com>
2020-09-29 23:02:25 +0100

    Swap ifttt service with meross service

--- services/service.device-manager/controllers/meross.js
+++ services/service.device-manager/controllers/meross.js
@@ -1,18 +1,18 @@
-const { SERVICE_IFTTT_URL } = process.env;
+const { SERVICE_MEROSS_URL } = process.env;
 
 module.exports = {
-  read(_, device) {
-    return {
-      name: device.name,
-      id: device.params.id,
-    };
+  async read(req, device) {
+    const { got } = req;
+    const { id } = device.params;
+
+    return got(`${SERVICE_MEROSS_URL}/devices/${id}`).json();
   },
 
   async write(req, device, state) {
     const { got } = req;
     const { id } = device.params;
     return got
-      .put(`${SERVICE_IFTTT_URL}/devices/${id}`, { json: { state } })
+      .put(`${SERVICE_MEROSS_URL}/devices/${id}`, { json: { state } })
       .json();
   },
 };

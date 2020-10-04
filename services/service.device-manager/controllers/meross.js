84079c06633a72fc01be3436553ea8a9b4aa49ed
Dan Hughes <danhughes@monzo.com>
2020-10-04 23:48:59 +0100

    Add channel support to device manager

--- services/service.device-manager/controllers/meross.js
+++ services/service.device-manager/controllers/meross.js
@@ -3,16 +3,23 @@ const { SERVICE_MEROSS_URL } = process.env;
 module.exports = {
   async read(req, device) {
     const { got } = req;
-    const { id } = device.params;
+    const { id, channel } = device.params;
 
-    return got(`${SERVICE_MEROSS_URL}/devices/${id}`).json();
+    const channelParameter = channel ? `/${channel}` : "";
+
+    return got(`${SERVICE_MEROSS_URL}/devices/${id}${channelParameter}`).json();
   },
 
   async write(req, device, state) {
     const { got } = req;
-    const { id } = device.params;
+    const { id, channel } = device.params;
+
+    const channelParameter = channel ? `/${channel}` : "";
+
     return got
-      .put(`${SERVICE_MEROSS_URL}/devices/${id}`, { json: { state } })
+      .put(`${SERVICE_MEROSS_URL}/devices/${id}${channelParameter}`, {
+        json: { state },
+      })
       .json();
   },
 };

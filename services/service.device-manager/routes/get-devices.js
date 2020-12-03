90926dd0fcfbb42fa8828627271efa6a48ff051e
Dan Hughes <danhughes@monzo.com>
2020-12-03 23:35:02 +0000

    Measure scene application and device state capture

--- services/service.device-manager/routes/get-devices.js
+++ services/service.device-manager/routes/get-devices.js
@@ -18,10 +18,31 @@ function flattenObjects(arrayOfObjects) {
   );
 }
 
+const finishSpan = (span) => (value) => {
+  span.finish();
+  return value;
+};
+
 module.exports = async (req, res) => {
+  const { Sentry } = req;
+
+  const transaction = Sentry.startTransaction({
+    op: "transaction",
+    name: `Store device states`,
+  });
+
   const responses = await Promise.all(
-    Object.entries(devices).map(([id, device]) => readDevice(req, id, device))
+    Object.entries(devices).map(([id, device]) => {
+      const span = transaction.startChild({
+        op: "read",
+        description: `Read device: ${device.name}`,
+      });
+
+      return readDevice(req, id, device).then(finishSpan(span));
+    })
   );
+
+  transaction.finish();
 
   return flattenObjects(responses);
 };

2216dba870b381d04bc995dac3453449db02528a
Dan Hughes <danhughes@monzo.com>
2021-04-06 16:32:31 +0100

    Don't call healthcheck if we failed to read the check

--- services/service.healthcheck/routes/put_finish.js
+++ services/service.healthcheck/routes/put_finish.js
@@ -2,6 +2,12 @@ module.exports = async (req, res) => {
   const { got } = req;
   const { token } = req.params;
 
+  // Failed to create Healthcheck - silently ignore as it'll show up as a Slack
+  // post anyway.
+  if (token === "_") {
+    return { ok: false };
+  }
+
   const url = `https://hc-ping.com/${token}`;
 
   // Not awaiting here, as we just fire and forget.
